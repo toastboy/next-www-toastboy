796415c2d55b0dc5cfc86bd1a3290c657904829b
Jon Wilson <github@toastboy.co.uk>
2024-01-14 16:38:03 +0000

    SYS-306 Small tweak to mugshot

--- src/components/PlayerMugshot.tsx
+++ src/components/PlayerMugshot.tsx
@@ -20,6 +20,7 @@ export default function PlayerMugshot({
                 width={250}
                 height={250}
                 src={url}
+                priority={true}
                 alt={getName(player) || "Player"}
             />
         </Link>

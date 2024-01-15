16bda7d76b9f6565bc829641e03c7e13bf171130
Jon Wilson <github@toastboy.co.uk>
2024-01-15 07:08:56 +0000

    SYS-310 Add some API routes

---
+++ src/app/footy/api/game_days/route.ts
@@ -0,0 +1,100 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: NextRequest) => {
+    const {
+        game_number,
+        game_date,
+        game,
+        mail_sent,
+        comment,
+        bibs,
+        picker_games_history,
+
+    } = await req.json();
+
+    const game_day = await prisma.game_day.create({
+        data: {
+            game_number,
+            game_date,
+            game,
+            mail_sent,
+            comment,
+            bibs,
+            picker_games_history,
+
+        },
+    });
+
+    return NextResponse.json({
+        game_day,
+    });
+};
+
+export const GET = async () => {
+    const game_days = await prisma.game_day.findMany({});
+
+    return NextResponse.json({
+        game_days,
+    });
+};
+
+export const PUT = async (req: NextRequest) => {
+    const {
+        game_number,
+        game_date,
+        game,
+        mail_sent,
+        comment,
+        bibs,
+        picker_games_history,
+
+    } = await req.json();
+
+    const game_day = await prisma.game_day.update({
+        where: {
+            game_number: Number(game_number),
+        },
+
+        data: {
+            game_number,
+            game_date,
+            game,
+            mail_sent,
+            comment,
+            bibs,
+            picker_games_history,
+
+        },
+    });
+
+    return NextResponse.json({
+        game_day,
+    });
+};
+
+export const DELETE = async (req: NextRequest) => {
+    const url = new URL(req.url).searchParams;
+    const game_number = Number(url.get("game_number")) || 0;
+
+    const game_day = await prisma.game_day.delete({
+        where: {
+            game_number: game_number,
+        },
+    });
+
+    if (!game_day) {
+        return NextResponse.json(
+            {
+                message: "Error",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+
+    return NextResponse.json({});
+};

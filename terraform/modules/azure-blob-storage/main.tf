3fda21a3ce0955bf8bcc1163fcbbff6c7ae6e40f
Jon Wilson <github@toastboy.co.uk>
2024-01-25 20:58:58 +0000

    SYS-310 Data block for the resource group

--- terraform/modules/azure-blob-storage/main.tf
+++ terraform/modules/azure-blob-storage/main.tf
@@ -1,14 +1,11 @@
-resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
-
-  tags = var.tags
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
 }
 
 resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = azurerm_resource_group.this.location
+  resource_group_name      = data.azurerm_resource_group.this.name
+  location                 = data.azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 

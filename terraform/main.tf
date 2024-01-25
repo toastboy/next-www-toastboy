eab4356f3a4426608c83256a8cde61f248a70b6d
Jon Wilson <github@toastboy.co.uk>
2024-01-25 21:16:53 +0000

    SYS-310 Add vault access policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,6 +47,19 @@ resource "azurerm_key_vault" "next_www_toastboy" {
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
+
   tags = var.tags
 }
 

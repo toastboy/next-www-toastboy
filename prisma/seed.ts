f5953d1bfffd033f4eadfe48270dc4bb4a8749e8
Jon Wilson <github@toastboy.co.uk>
2024-01-28 11:53:24 +0000

    SYS-310 Seed database from Azure Blobl Storage

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,75 +1,101 @@
 import { arse, club_supporter, club, country, game_chat, game_day, invitation, nationality, outcome, player, standings } from '@prisma/client'
 import prisma from '../src/lib/prisma'
+import { Prisma } from '@prisma/client';
+import { BlobServiceClient, ContainerClient } from '@azure/storage-blob';
+import { ClientSecretCredential } from '@azure/identity';
 
-import fs from 'fs';
-import path from 'path';
+async function downloadAndParseJson(containerClient: ContainerClient, blobName: string): Promise<unknown> {
+    try {
+        const blobClient = containerClient.getBlobClient(blobName);
+        const downloadBlockBlobResponse = await blobClient.download(0);
+
+        if (!downloadBlockBlobResponse.readableStreamBody) {
+            throw new Error("Reading " + blobName + " failed");
+        }
+
+        const downloadedContent = (await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)).toString();
+
+        return JSON.parse(downloadedContent);
+    } catch (error) {
+        console.error("An error occurred during download: ", error);
+        throw error;
+    }
+}
+
+async function streamToBuffer(readableStream: NodeJS.ReadableStream): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const chunks: Buffer[] = [];
+        readableStream.on('data', (data: Buffer | string) => {
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on('end', () => {
+            resolve(Buffer.concat(chunks));
+        });
+        readableStream.on('error', reject);
+    });
+}
+
+async function processJsonData<T>(
+    containerClient: ContainerClient,
+    fileName: string,
+    prismaModel: {
+        deleteMany: () => Prisma.PrismaPromise<Prisma.BatchPayload>;
+        create: (data: { data: T }) => Prisma.PrismaPromise<T>;
+    }
+) {
+    const dataItems: T[] = await downloadAndParseJson(containerClient, fileName) as T[];
+    await prismaModel.deleteMany();
+    for (const item of dataItems) {
+        await prismaModel.create({ data: item });
+    }
+    console.log("Complete: " + fileName);
+}
 
 async function main() {
-    const arses: arse[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/arses.json'), 'utf-8'));
-
-    for (const arse of arses) {
-        await prisma.arse.create({ data: arse });
+    const tenantId = process.env.AZURE_TENANT_ID;
+    if (!tenantId) {
+        throw new Error('AZURE_TENANT_ID undefined');
     }
 
-    const club: club_supporter[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/club_supporters.json'), 'utf-8'));
-
-    for (const club_supporter of club) {
-        await prisma.club_supporter.create({ data: club_supporter });
+    const clientId = process.env.AZURE_CLIENT_ID;
+    if (!clientId) {
+        throw new Error('AZURE_CLIENT_ID undefined');
     }
 
-    const clubs: club[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/clubs.json'), 'utf-8'));
-
-    for (const club of clubs) {
-        await prisma.club.create({ data: club });
+    const clientSecret = process.env.AZURE_CLIENT_SECRET;
+    if (!clientSecret) {
+        throw new Error('AZURE_CLIENT_SECRET undefined');
     }
 
-    const countries: country[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/countries.json'), 'utf-8'));
-
-    for (const country of countries) {
-        await prisma.country.create({ data: country });
+    const storageAccountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+    if (!storageAccountName) {
+        throw new Error('AZURE_STORAGE_ACCOUNT_NAME undefined');
     }
 
-    const game_chats: game_chat[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/game_chats.json'), 'utf-8'));
-
-    for (const game_chat of game_chats) {
-        await prisma.game_chat.create({ data: game_chat });
+    const containerName = process.env.AZURE_CONTAINER_NAME;
+    if (!containerName) {
+        throw new Error('AZURE_CONTAINER_NAME undefined');
     }
 
-    const game_days: game_day[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/game_days.json'), 'utf-8'));
+    const credentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    const blobServiceClient = new BlobServiceClient(
+        `https://${storageAccountName}.blob.core.windows.net`,
+        credentials
+    );
 
-    for (const game_day of game_days) {
-        await prisma.game_day.create({ data: game_day });
-    }
+    const containerClient = blobServiceClient.getContainerClient(containerName);
 
-    const invitations: invitation[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/invitations.json'), 'utf-8'));
-
-    for (const invitation of invitations) {
-        await prisma.invitation.create({ data: invitation });
-    }
-
-    const nationalities: nationality[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/nationalities.json'), 'utf-8'));
-
-    for (const nationality of nationalities) {
-        await prisma.nationality.create({ data: nationality });
-    }
-
-    const outcomes: outcome[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/outcomes.json'), 'utf-8'));
-
-    for (const outcome of outcomes) {
-        await prisma.outcome.create({ data: outcome });
-    }
-
-    const players: player[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/players.json'), 'utf-8'));
-
-    for (const player of players) {
-        await prisma.player.create({ data: player });
-    }
-
-    const standings: standings[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data/standings.json'), 'utf-8'));
-
-    for (const standing of standings) {
-        await prisma.standings.create({ data: standing });
-    }
+    await processJsonData<arse>(containerClient, "arses.json", prisma.arse);
+    await processJsonData<club_supporter>(containerClient, "club_supporters.json", prisma.club_supporter);
+    await processJsonData<club>(containerClient, "clubs.json", prisma.club);
+    await processJsonData<country>(containerClient, "countries.json", prisma.country);
+    await processJsonData<game_chat>(containerClient, "game_chatss.json", prisma.game_chat);
+    await processJsonData<game_day>(containerClient, "game_days.json", prisma.game_day);
+    await processJsonData<invitation>(containerClient, "invitations.json", prisma.invitation);
+    await processJsonData<nationality>(containerClient, "nationalities.json", prisma.nationality);
+    await processJsonData<outcome>(containerClient, "outcomes.json", prisma.outcome);
+    await processJsonData<player>(containerClient, "players.json", prisma.player);
+    await processJsonData<standings>(containerClient, "standings.json", prisma.standings);
 }
 
 main()

b82abc72ffb7894bc2a2d57ded6bae60a3d7653f
Jon Wilson <github@toastboy.co.uk>
2024-03-03 07:38:23 +0000

    SYS-306 Add Invitation and tests

---
+++ src/services/Invitation.ts
@@ -0,0 +1,133 @@
+import { Invitation } from '@prisma/client';
+import prisma from 'lib/prisma';
+import debug from 'debug';
+
+const log = debug('footy:api');
+
+export class InvitationService {
+    /**
+     * Validate a Invitation
+     * @param invitation The Invitation to validate
+     * @returns the validated Invitation
+     * @throws An error if the Invitation is invalid.
+     */
+    validate(invitation: Invitation): Invitation {
+        if (!invitation.uuid || typeof invitation.uuid !== 'string' || invitation.uuid === '') {
+            throw new Error(`Invalid uuid value: ${invitation.uuid}`);
+        }
+        if (!invitation.player || !Number.isInteger(invitation.player) || invitation.player < 0) {
+            throw new Error(`Invalid player value: ${invitation.player}`);
+        }
+        if (!invitation.game_day || !Number.isInteger(invitation.game_day) || invitation.game_day < 0) {
+            throw new Error(`Invalid game_day value: ${invitation.game_day}`);
+        }
+
+        return invitation;
+    }
+
+    /**
+     * Retrieves a Invitation by its ID.
+     * @param uuid - The ID of the Invitation to retrieve.
+     * @returns A Promise that resolves to the Invitation object if found, or null if not found.
+     * @throws If there is an error while fetching the Invitation.
+     */
+    async get(uuid: string): Promise<Invitation | null> {
+        try {
+            return prisma.invitation.findUnique({
+                where: {
+                    uuid: uuid
+                },
+            });
+        } catch (error) {
+            log(`Error fetching Invitation: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Retrieves all Invitation.
+     * @returns A promise that resolves to an array of Invitations or null if an error occurs.
+     * @throws An error if there is a failure.
+     */
+    async getAll(): Promise<Invitation[] | null> {
+        try {
+            return prisma.invitation.findMany({});
+        } catch (error) {
+            log(`Error fetching Invitation: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Creates a new invitation.
+     * @param data The data for the new invitation.
+     * @returns A promise that resolves to the created invitation, or null if an error occurs.
+     * @throws An error if there is a failure.
+     */
+    async create(data: Invitation): Promise<Invitation | null> {
+        try {
+            return await prisma.invitation.create({
+                data: this.validate(data)
+            });
+        } catch (error) {
+            log(`Error creating Invitation: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Upserts a invitation.
+     * @param data The data to be upserted.
+     * @returns A promise that resolves to the upserted invitation, or null if the upsert failed.
+     * @throws An error if there is a failure.
+     */
+    async upsert(data: Invitation): Promise<Invitation | null> {
+        try {
+            return await prisma.invitation.upsert({
+                where: {
+                    uuid: data.uuid
+                },
+                update: data,
+                create: data,
+            });
+        } catch (error) {
+            log(`Error upserting Invitation: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Deletes a invitation by its ID.
+     * @param uuid - The ID of the invitation to delete.
+     * @throws If there is an error deleting the invitation.
+     */
+    async delete(uuid: string): Promise<void> {
+        try {
+            await prisma.invitation.delete({
+                where: {
+                    uuid: uuid
+                },
+            });
+        } catch (error) {
+            log(`Error deleting Invitation: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Deletes all invitations.
+     * @returns A promise that resolves when all invitations are deleted.
+     * @throws An error if there is a failure.
+     */
+    async deleteAll(): Promise<void> {
+        try {
+            await prisma.invitation.deleteMany();
+        } catch (error) {
+            log(`Error deleting Invitations: ${error}`);
+            throw error;
+        }
+    }
+}
+
+const invitationService = new InvitationService();
+export default invitationService;
---
+++ tests/Invitation.test.ts
@@ -0,0 +1,171 @@
+import { Invitation } from '@prisma/client';
+import invitationService from 'services/Invitation';
+import prisma from 'lib/prisma';
+
+jest.mock('lib/prisma', () => ({
+    invitation: {
+        findUnique: jest.fn(),
+        findMany: jest.fn(),
+        create: jest.fn(),
+        upsert: jest.fn(),
+        delete: jest.fn(),
+        deleteMany: jest.fn(),
+    },
+}));
+
+const defaultInvitation: Invitation = {
+    uuid: '1234',
+    player: 1,
+    game_day: 1,
+};
+
+const invalidInvitation: Invitation = {
+    ...defaultInvitation,
+    uuid: '',
+};
+
+const invitationList: Invitation[] = Array.from({ length: 100 }, (_, index) => ({
+    ...defaultInvitation,
+    uuid: `1234${index + 1}`,
+}));
+
+describe('InvitationService', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        (prisma.invitation.findUnique as jest.Mock).mockImplementation((args: {
+            where: { uuid: string }
+        }) => {
+            const invitation = invitationList.find((invitation) => invitation.uuid === args.where.uuid);
+            return Promise.resolve(invitation ? invitation : null);
+        });
+
+        (prisma.invitation.create as jest.Mock).mockImplementation((args: { data: Invitation }) => {
+            const invitation = invitationList.find((invitation) => invitation.uuid === args.data.uuid);
+
+            if (invitation) {
+                return Promise.reject(new Error('invitation already exists'));
+            }
+            else {
+                return Promise.resolve(args.data);
+            }
+        });
+
+        (prisma.invitation.upsert as jest.Mock).mockImplementation((args: {
+            where: { uuid: string },
+            update: Invitation,
+            create: Invitation,
+        }) => {
+            const invitation = invitationList.find((invitation) => invitation.uuid === args.where.uuid);
+
+            if (invitation) {
+                return Promise.resolve(args.update);
+            }
+            else {
+                return Promise.resolve(args.create);
+            }
+        });
+
+        (prisma.invitation.delete as jest.Mock).mockImplementation((args: {
+            where: { uuid }
+        }) => {
+            const invitation = invitationList.find((invitation) => invitation.uuid === args.where.uuid);
+            return Promise.resolve(invitation ? invitation : null);
+        });
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('get', () => {
+        it('should retrieve the correct Invitation with uuid "12346"', async () => {
+            const result = await invitationService.get('12346');
+            expect(result).toEqual({
+                ...defaultInvitation,
+                uuid: '12346',
+            } as Invitation);
+        });
+
+        it('should return null for uuid "6789"', async () => {
+            const result = await invitationService.get('6789');
+            expect(result).toBeNull();
+        });
+    });
+
+    describe('getAll', () => {
+        beforeEach(() => {
+            (prisma.invitation.findMany as jest.Mock).mockImplementation(() => {
+                return Promise.resolve(invitationList);
+            });
+        });
+
+        it('should return the correct, complete list of 100 Invitation', async () => {
+            const result = await invitationService.getAll();
+            expect(result.length).toEqual(100);
+            expect(result[11].uuid).toEqual('123412');
+        });
+    });
+
+    describe('create', () => {
+        it('should create a Invitation', async () => {
+            const newInvitation: Invitation = {
+                ...defaultInvitation,
+                uuid: '4567',
+            };
+            const result = await invitationService.create(newInvitation);
+            expect(result).toEqual(newInvitation);
+        });
+
+        it('should refuse to create a Invitation with invalid data', async () => {
+            await expect(invitationService.create(invalidInvitation)).rejects.toThrow();
+        });
+
+        it('should refuse to create a Invitation that has the same uuid as an existing one', async () => {
+            await expect(invitationService.create({
+                ...defaultInvitation,
+                uuid: '12341',
+            })).rejects.toThrow();
+        });
+    });
+
+    describe('upsert', () => {
+        it('should create a Invitation', async () => {
+            const result = await invitationService.upsert(defaultInvitation);
+            expect(result).toEqual(defaultInvitation);
+        });
+
+        it('should update an existing Invitation where one with the uuid already existed', async () => {
+            const updatedInvitation: Invitation = {
+                ...defaultInvitation,
+                uuid: '1234',
+            };
+            const result = await invitationService.upsert(updatedInvitation);
+            expect(result).toEqual(updatedInvitation);
+        });
+
+        it('should refuse to create a Invitation with invalid data where one with the uuid did not exist', async () => {
+            await expect(invitationService.create(invalidInvitation)).rejects.toThrow();
+        });
+
+        it('should refuse to update a Invitation with invalid data where one with the uuid already existed', async () => {
+            await expect(invitationService.create(invalidInvitation)).rejects.toThrow();
+        });
+    });
+
+    describe('delete', () => {
+        it('should delete an existing Invitation', async () => {
+            await invitationService.delete('1234');
+        });
+
+        it('should silently return when asked to delete a Invitation that does not exist', async () => {
+            await invitationService.delete('6789');
+        });
+    });
+
+    describe('deleteAll', () => {
+        it('should delete all Invitations', async () => {
+            await invitationService.deleteAll();
+        });
+    });
+});

c294e28783ffafd1a993760df7f3f5eac79a093c
Jon Wilson <github@toastboy.co.uk>
2024-03-16 09:21:10 +0000

    SYS-325 Node update and GRS storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_storage_account" "next_www_toastboy" {
   resource_group_name      = azurerm_resource_group.next_www_toastboy.name
   location                 = azurerm_resource_group.next_www_toastboy.location
   account_tier             = "Standard"
-  account_replication_type = "ZRS"
+  account_replication_type = "GRS"
   min_tls_version          = "TLS1_2"
 
   blob_properties {

63475bd4e8dcedf369b8e7a29cea0d864dc5e04f
Jon Wilson <github@toastboy.co.uk>
2024-04-04 14:25:46 +0100

    SYS-306 Add player links to table

--- src/components/TablePoints.tsx
+++ src/components/TablePoints.tsx
@@ -1,6 +1,7 @@
 import playerRecordService from 'services/PlayerRecord';
 import playerService from 'services/Player';
 import { EnumTable } from 'services/PlayerRecord';
+import Link from 'next/link';
 
 export default async function TablePoints({ year, take }: { year: number, take?: number }) {
     const playerRecords = await playerRecordService.getTable(EnumTable.points, year, take);
@@ -12,7 +13,11 @@ export default async function TablePoints({ year, take }: { year: number, take?:
     return (
         <div className="px-6 py-4">
             {playerRecords.map((playerRecord, index) => (
-                <p key={index} className="text-gray-700 text-base">{playerService.getName(playerRecord.player)} : {playerRecord.points}</p>
+                <div key={index}>
+                    <Link className="text-gray-700 text-base" href={'/footy/player/' + playerRecord.playerId.toString()} >
+                        {playerService.getName(playerRecord.player)} : {playerRecord.points}
+                    </Link>
+                </div>
             ))}
         </div>
     );

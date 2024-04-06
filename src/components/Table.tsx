c8b153b3fa5530489a5f3b7950c95723c329514a
Jon Wilson <github@toastboy.co.uk>
2024-04-06 07:04:21 +0100

    SYS-336 Add per-table score mechanism

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,7 +12,23 @@ export default async function Table({ table, year, take }: { table: EnumTable, y
         <div className="px-6 py-4">
             {playerRecords.map((playerRecord, index) => (
                 <div key={index}>
-                    <PlayerLink idOrLogin={playerRecord.playerId.toString()} /> : {playerRecord[table]}
+                    <PlayerLink idOrLogin={playerRecord.playerId.toString()} /> : {(() => {
+                        switch (table) {
+                            case EnumTable.averages:
+                                return `${playerRecord.averages?.toFixed(3)}`;
+                            case EnumTable.speedy:
+                                if (playerRecord.speedy) {
+                                    const date = new Date(0);
+                                    date.setSeconds(playerRecord.speedy);
+                                    return date.toISOString().substring(11, 19);
+                                }
+                                else {
+                                    return null;
+                                }
+                            default:
+                                return playerRecord[table];
+                        }
+                    })()}
                 </div>
             ))}
         </div>

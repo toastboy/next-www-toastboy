afe2910f046f33c452be59c14ea175af3acdac1e
Jon Wilson <github@toastboy.co.uk>
2024-04-07 18:19:22 +0100

    SYS-345 Add games page

--- src/app/footy/games/page.tsx
+++ src/app/footy/games/page.tsx
@@ -1,7 +1,10 @@
-import { NYI } from "components/NYI";
+import gameDayService from "services/GameDay";
 
-export default function Page() {
+export default async function Page() {
     return (
-        <NYI />
+        <div>
+            <p>{await gameDayService.getGamesPlayed(0)} games played to date.</p>
+            <p>{await gameDayService.getGamesRemaining(0)} future games confirmed.</p>
+        </div>
     );
 }

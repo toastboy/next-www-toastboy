3f34fe7fad5e8f88d1091457d700a4ba5cf3742d
Jon Wilson <github@toastboy.co.uk>
2024-05-19 08:27:40 +0100

    SYS-374 Add FloatingIndicator to GameYears

--- src/components/GameYears.tsx
+++ src/components/GameYears.tsx
@@ -1,20 +1,46 @@
 'use client';
 
-import { Loader } from '@mantine/core';
+import { FloatingIndicator, Loader, UnstyledButton } from '@mantine/core';
 import { useGameYears } from 'lib/swr';
+import { useState } from 'react';
+import classes from './GameYears.module.css';
 
 export default function GameYears() {
     const { data, error, isLoading } = useGameYears();
+    const [rootRef, setRootRef] = useState<HTMLDivElement | null>(null);
+    const [controlsRefs, setControlsRefs] = useState<Record<string, HTMLButtonElement | null>>({});
+    const [active, setActive] = useState(0);
 
     if (error) return <div>failed to load</div>;
     if (isLoading) return <Loader color="gray" type="dots" />;
     if (!data) return null;
 
+    const setControlRef = (index: number) => (node: HTMLButtonElement) => {
+        controlsRefs[index] = node;
+        setControlsRefs(controlsRefs);
+    };
+
+    const controls = data.map((year, index) => (
+        <UnstyledButton
+            key={year}
+            className={classes.control}
+            ref={setControlRef(index)}
+            onClick={() => setActive(index)}
+            mod={{ active: active === index }}
+        >
+            <span className={classes.controlLabel}>{year}</span>
+        </UnstyledButton>
+    ));
+
     return (
-        <div className="px-6 py-4">
-            {data.map((year, index) => (
-                <p key={index} className="text-gray-700 text-base">{year}</p>
-            ))}
+        <div className={classes.root} ref={setRootRef}>
+            {controls}
+
+            <FloatingIndicator
+                target={controlsRefs[active]}
+                parent={rootRef}
+                className={classes.indicator}
+            />
         </div>
     );
 }

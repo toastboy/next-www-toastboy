14fe1a119c1afbf3891ef7c82b6dd6392d767e22
Jon Wilson <github@toastboy.co.uk>
2024-06-08 20:28:30 +0100

    SYS-382 Add PlayerArse test

--- src/components/PlayerArse.tsx
+++ src/components/PlayerArse.tsx
@@ -8,24 +8,20 @@ interface PlayerArseProps {
 }
 
 const PlayerArse: React.FC<PlayerArseProps> = ({ idOrLogin }) => {
-    const { data: arse, error, isLoading } = usePlayerArse(idOrLogin);
+    const { data, error, isLoading } = usePlayerArse(idOrLogin);
 
-    if (error) return <div>failed to load</div>;
     if (isLoading) return <Loader color="gray" type="dots" />;
-
-    if (!arse) {
-        return null;
-    }
+    if (error || !data) return <div>failed to load</div>;
 
     return (
         <div className="px-6 py-4">
-            <p className="text-gray-700 text-base">In Goal: {arse.in_goal}</p>
-            <p className="text-gray-700 text-base">Running: {arse.running}</p>
-            <p className="text-gray-700 text-base">Shooting: {arse.shooting}</p>
-            <p className="text-gray-700 text-base">Passing: {arse.passing}</p>
-            <p className="text-gray-700 text-base">Ball Skill: {arse.ball_skill}</p>
-            <p className="text-gray-700 text-base">Attacking: {arse.attacking}</p>
-            <p className="text-gray-700 text-base">Defending: {arse.defending}</p>
+            <p className="text-gray-700 text-base">In Goal: {data.in_goal}</p>
+            <p className="text-gray-700 text-base">Running: {data.running}</p>
+            <p className="text-gray-700 text-base">Shooting: {data.shooting}</p>
+            <p className="text-gray-700 text-base">Passing: {data.passing}</p>
+            <p className="text-gray-700 text-base">Ball Skill: {data.ball_skill}</p>
+            <p className="text-gray-700 text-base">Attacking: {data.attacking}</p>
+            <p className="text-gray-700 text-base">Defending: {data.defending}</p>
         </div>
     );
 };
--- tests/components/PlayerArse.test.tsx
+++ tests/components/PlayerArse.test.tsx
@@ -1,55 +1,66 @@
 import { render, screen } from '@testing-library/react';
-import NYI from 'components/NYI';
+import PlayerArse from 'components/PlayerArse';
 import { Wrapper, errorText, loaderClass } from "./lib/common";
-// import { useNYI } from 'lib/swr';
+import { usePlayerArse } from 'lib/swr';
 
 jest.mock('lib/swr');
 
-describe('NYI', () => {
-    it('renders loading state', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: undefined,
-        //     error: undefined,
-        //     isLoading: true,
-        // });
+describe('PlayerArse', () => {
+    const idOrLogin = 'derekt';
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+    it('renders loading state', () => {
+        (usePlayerArse as jest.Mock).mockReturnValue({
+            data: undefined,
+            error: undefined,
+            isLoading: true,
+        });
+
+        const { container } = render(<Wrapper><PlayerArse idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).toBeInTheDocument();
     });
 
     it('renders error state', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: undefined,
-        //     error: new Error(errorText),
-        //     isLoading: false,
-        // });
+        (usePlayerArse as jest.Mock).mockReturnValue({
+            data: undefined,
+            error: new Error(errorText),
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerArse idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
         expect(screen.getByText(errorText)).toBeInTheDocument();
     });
 
     it('renders error state when data is null', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: null,
-        //     error: undefined,
-        //     isLoading: false,
-        // });
+        (usePlayerArse as jest.Mock).mockReturnValue({
+            data: null,
+            error: undefined,
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerArse idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
         expect(screen.getByText(errorText)).toBeInTheDocument();
     });
 
     it('renders with data', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: [2001, 2002, 2003, 0],
-        //     error: undefined,
-        //     isLoading: false,
-        // });
+        (usePlayerArse as jest.Mock).mockReturnValue({
+            data: {
+                in_goal: 1,
+                running: 2,
+                shooting: 3,
+                passing: 4,
+                ball_skill: 5,
+                attacking: 6,
+                defending: 7,
+            },
+            error: undefined,
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerArse idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
-        expect(screen.getByText("NYI")).toBeInTheDocument();
+        expect(screen.getByText("In Goal: 1")).toBeInTheDocument();
+        expect(screen.getByText("Defending: 7")).toBeInTheDocument();
     });
 });

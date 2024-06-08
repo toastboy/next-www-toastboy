41c7822c6fed0626999ce32b3b9a324fad957b9e
Jon Wilson <github@toastboy.co.uk>
2024-06-08 20:21:01 +0100

    SYS-382 Add PlayerClubs test

--- src/components/PlayerClubs.tsx
+++ src/components/PlayerClubs.tsx
@@ -4,24 +4,19 @@ import { usePlayerClubs } from 'lib/swr';
 import ClubBadge from 'components/ClubBadge';
 import { Loader } from '@mantine/core';
 
-
 interface PlayerClubsProps {
     idOrLogin: string;
 }
 
 const PlayerClubs: React.FC<PlayerClubsProps> = ({ idOrLogin }) => {
-    const { data: clubs, error, isLoading } = usePlayerClubs(idOrLogin);
+    const { data, error, isLoading } = usePlayerClubs(idOrLogin);
 
-    if (error) return <div>failed to load</div>;
     if (isLoading) return <Loader color="gray" type="dots" />;
-
-    if (!clubs || clubs.length === 0) {
-        return null;
-    }
+    if (error || !data || data.length === 0) return <div>failed to load</div>;
 
     return (
         <div className="px-6 py-4">
-            {clubs.map((item: number) => (
+            {data.map((item: number) => (
                 <ClubBadge key={item} clubId={item} />
             ))}
         </div>
--- tests/components/PlayerClubs.test.tsx
+++ tests/components/PlayerClubs.test.tsx
@@ -1,55 +1,65 @@
 import { render, screen } from '@testing-library/react';
-import NYI from 'components/NYI';
+import PlayerClubs from 'components/PlayerClubs';
+import { usePlayerClubs } from 'lib/swr';
 import { Wrapper, errorText, loaderClass } from "./lib/common";
-// import { useNYI } from 'lib/swr';
 
 jest.mock('lib/swr');
+jest.mock('components/ClubBadge', () => {
+    const ClubBadge = ({ clubId }: { clubId: number }) => (
+        <div>ClubBadge (clubId: {clubId})</div>
+    );
+    ClubBadge.displayName = 'ClubBadge';
+    return ClubBadge;
+});
 
-describe('NYI', () => {
+describe('PlayerClubs', () => {
+    const idOrLogin = "160";
+
     it('renders loading state', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: undefined,
-        //     error: undefined,
-        //     isLoading: true,
-        // });
+        (usePlayerClubs as jest.Mock).mockReturnValue({
+            data: undefined,
+            error: undefined,
+            isLoading: true,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerClubs idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).toBeInTheDocument();
     });
 
     it('renders error state', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: undefined,
-        //     error: new Error(errorText),
-        //     isLoading: false,
-        // });
+        (usePlayerClubs as jest.Mock).mockReturnValue({
+            data: undefined,
+            error: new Error(errorText),
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerClubs idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
         expect(screen.getByText(errorText)).toBeInTheDocument();
     });
 
     it('renders error state when data is null', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: null,
-        //     error: undefined,
-        //     isLoading: false,
-        // });
+        (usePlayerClubs as jest.Mock).mockReturnValue({
+            data: null,
+            error: undefined,
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerClubs idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
         expect(screen.getByText(errorText)).toBeInTheDocument();
     });
 
     it('renders with data', () => {
-        // (useNYI as jest.Mock).mockReturnValue({
-        //     data: [2001, 2002, 2003, 0],
-        //     error: undefined,
-        //     isLoading: false,
-        // });
+        (usePlayerClubs as jest.Mock).mockReturnValue({
+            data: [1000, 2000],
+            error: undefined,
+            isLoading: false,
+        });
 
-        const { container } = render(<Wrapper><NYI /></Wrapper>);
+        const { container } = render(<Wrapper><PlayerClubs idOrLogin={idOrLogin} /></Wrapper>);
         expect(container.querySelector(loaderClass)).not.toBeInTheDocument();
-        expect(screen.getByText("NYI")).toBeInTheDocument();
+        expect(screen.getByText("ClubBadge (clubId: 1000)")).toBeInTheDocument();
+        expect(screen.getByText("ClubBadge (clubId: 2000)")).toBeInTheDocument();
     });
 });

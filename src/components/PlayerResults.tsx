5e7527432f4ee263a2a3b787d7c93d68315f1043
Jon Wilson <github@toastboy.co.uk>
2024-06-08 12:01:48 +0100

    SYS-382 Add PlayerResults test

--- src/components/PlayerResults.tsx
+++ src/components/PlayerResults.tsx
@@ -10,24 +10,20 @@ interface PlayerResultsProps {
 }
 
 const PlayerResults: React.FC<PlayerResultsProps> = ({ idOrLogin, year }) => {
-    const { data: record, error, isLoading } = usePlayerRecord(idOrLogin, year);
+    const { data, error, isLoading } = usePlayerRecord(idOrLogin, year);
 
-    if (error) return <div>failed to load</div>;
     if (isLoading) return <Loader color="gray" type="dots" />;
-
-    if (!record) {
-        return null;
-    }
+    if (error || !data) return <div>failed to load</div>;
 
     return (
         <div className="px-6 py-4">
-            <table summary={`${record.name}'s ${getYearName(year)} results record`}>
+            <table summary={`${data.name}'s ${getYearName(year)} results record`}>
                 <caption>{getYearName(year)} Results</caption>
                 <tbody>
-                    <tr><th>Played</th><td>{record.P}</td></tr>
-                    <tr><th>Won</th><td>{record.W}</td></tr>
-                    <tr><th>Drawn</th><td>{record.D}</td></tr>
-                    <tr><th>Lost</th><td>{record.L}</td></tr>
+                    <tr><th>Played</th><td>{data.P}</td></tr>
+                    <tr><th>Won</th><td>{data.W}</td></tr>
+                    <tr><th>Drawn</th><td>{data.D}</td></tr>
+                    <tr><th>Lost</th><td>{data.L}</td></tr>
                 </tbody>
             </table>
         </div>

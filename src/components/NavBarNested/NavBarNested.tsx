f8f3f4efc0904b424c85e239b283c0f8cc458cb9
Jon Wilson <github@toastboy.co.uk>
2024-06-12 13:47:30 +0100

    SYS-382 Remove JSON from NavBarNested

--- src/components/NavBarNested/NavBarNested.tsx
+++ src/components/NavBarNested/NavBarNested.tsx
@@ -2,12 +2,82 @@
 
 import { ScrollArea } from '@mantine/core';
 import NavbarLinksGroup from 'components/NavbarLinksGroup/NavbarLinksGroup';
-import data from './NavbarNested.json';
 import classes from './NavbarNested.module.css';
 import UserButton from 'components/UserButton/UserButton';
 
 const NavbarNested: React.FC = () => {
-    const links = data.map((item) => <NavbarLinksGroup {...item} key={item.label} />);
+    const links = [
+        {
+            "label": "Games",
+            "initiallyOpened": true,
+            "links": [
+                {
+                    "label": "Next Game",
+                    "link": "/footy/nextgame",
+                },
+                {
+                    "label": "Results",
+                    "link": "/footy/results",
+                },
+                {
+                    "label": "Fixtures",
+                    "link": "/footy/fixtures",
+                },
+            ],
+        },
+        {
+            "label": "Tables",
+            "initiallyOpened": true,
+            "links": [
+                {
+                    "label": "Winners",
+                    "link": "/footy/winners",
+                },
+                {
+                    "label": "Points",
+                    "link": "/footy/points",
+                },
+                {
+                    "label": "Averages",
+                    "link": "/footy/averages",
+                },
+                {
+                    "label": "Stalwart Standings",
+                    "link": "/footy/stalwart",
+                },
+                {
+                    "label": "Captain Speedy",
+                    "link": "/footy/speedy",
+                },
+                {
+                    "label": "Pub",
+                    "link": "/footy/pub",
+                },
+            ],
+        },
+        {
+            "label": "Players",
+            "initiallyOpened": true,
+            "links": [
+                {
+                    "label": "Info",
+                    "link": "/footy/info",
+                },
+                {
+                    "label": "Player List",
+                    "link": "/footy/players",
+                },
+                {
+                    "label": "Turnout",
+                    "link": "/footy/turnout",
+                },
+                {
+                    "label": "Rules",
+                    "link": "/footy/rules",
+                },
+            ],
+        },
+    ];
 
     return (
         <nav className={classes.navbar}>
@@ -15,7 +85,7 @@ const NavbarNested: React.FC = () => {
             </div>
 
             <ScrollArea className={classes.links}>
-                <div className={classes.linksInner}>{links}</div>
+                <div className={classes.linksInner}>{links.map((item) => <NavbarLinksGroup {...item} key={item.label} />)}</div>
             </ScrollArea>
 
             <div className={classes.footer}>

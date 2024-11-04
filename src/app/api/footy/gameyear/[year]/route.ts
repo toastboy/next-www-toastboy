af5573141bac934c4cc95caea23a859d98ceaa45
Jon Wilson <github@toastboy.co.uk>
2024-11-04 20:35:40 +0000

    SYS-383 Add gameyear tests

--- src/app/api/footy/gameyear/[year]/route.ts
+++ src/app/api/footy/gameyear/[year]/route.ts
@@ -1,8 +1,10 @@
-import gameDayService from 'services/GameDay';
 import { handleGET } from 'lib/api';
+import gameDayService from 'services/GameDay';
 
 export const GET = (request: Request, { params }: { params: Record<string, string> }) =>
     handleGET(() => {
         if (parseInt(params.year) === 0) return Promise.resolve(true);
-        return gameDayService.getYear(parseInt(params.year)).then((year) => Promise.resolve(year == parseInt(params.year) ? true : null));
+        return gameDayService.getYear(
+            parseInt(params.year)).then((year) =>
+                Promise.resolve(year == parseInt(params.year) ? true : null));
     }, { params });

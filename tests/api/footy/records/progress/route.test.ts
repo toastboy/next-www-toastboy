f68e2fac273a771289d4bbb566698614207a578c
Jon Wilson <github@toastboy.co.uk>
2024-11-08 12:33:50 +0000

    SYS-383 Add records progress API test

---
+++ tests/api/footy/records/progress/route.test.ts
@@ -0,0 +1,41 @@
+import { GET } from 'api/footy/records/progress/route';
+import playerRecordService from 'services/PlayerRecord';
+import request from 'supertest';
+import { createMockApp, jsonResponseHandler, suppressConsoleError } from 'tests/lib/api/common';
+
+suppressConsoleError();
+const mockURI = '/api/footy/records/progress';
+const mockApp = createMockApp(GET, { path: mockURI, params: {} }, jsonResponseHandler);
+
+jest.mock('services/PlayerRecord');
+
+describe('API tests using HTTP', () => {
+    it('should return JSON response', async () => {
+        const mockData = [50, 100];
+        (playerRecordService.getProgress as jest.Mock).mockResolvedValue(mockData);
+
+        const response = await request(mockApp).get(mockURI);
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+        expect(response.body).toEqual(mockData);
+    });
+
+    it('should return 404 if there is no progress', async () => {
+        (playerRecordService.getProgress as jest.Mock).mockResolvedValue(null);
+
+        const response = await request(mockApp).get(mockURI);
+
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Not Found');
+    });
+
+    it('should return 500 if there is an error', async () => {
+        (playerRecordService.getProgress as jest.Mock).mockRejectedValue(new Error('Test Error'));
+
+        const response = await request(mockApp).get(mockURI);
+
+        expect(response.status).toBe(500);
+        expect(response.text).toBe('Internal Server Error');
+    });
+});

52629cb0ce600ed9a561757eec9269973851a3bf
Jon Wilson <github@toastboy.co.uk>
2024-11-08 07:06:26 +0000

    SYS-383 Add player yearsactive API test

---
+++ tests/api/footy/player/[idOrLogin]/yearsactive/route.test.ts
@@ -0,0 +1,54 @@
+import { createMockApp, jsonResponseHandler, suppressConsoleError } from 'tests/lib/api/common';
+import { setupPlayerMocks } from 'tests/lib/api/player';
+
+jest.mock('services/Player');
+
+import { GET } from 'api/footy/player/[idOrLogin]/yearsactive/route';
+import playerService from 'services/Player';
+import request from 'supertest';
+
+suppressConsoleError();
+const testURI = '/api/footy/player/1/yearsactive';
+const mockApp = createMockApp(GET, { path: testURI, params: { idOrLogin: "1" } }, jsonResponseHandler);
+
+describe('API tests using HTTP', () => {
+    setupPlayerMocks();
+
+    it('should return JSON response for a valid player', async () => {
+        const mockData = [2001, 2002, 2003, 0];
+        (playerService.getYearsActive as jest.Mock).mockResolvedValue(mockData);
+
+        const response = await request(mockApp).get(testURI);
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+        expect(response.body).toEqual(mockData);
+    });
+
+    it('should return 404 if the player does not exist', async () => {
+        (playerService.getByIdOrLogin as jest.Mock).mockResolvedValue(null);
+
+        const response = await request(mockApp).get(testURI);
+
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Not Found');
+    });
+
+    it('should return 404 if the arse record does not exist', async () => {
+        (playerService.getYearsActive as jest.Mock).mockResolvedValue(null);
+
+        const response = await request(mockApp).get(testURI);
+
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Not Found');
+    });
+
+    it('should return 500 if there is an error', async () => {
+        (playerService.getYearsActive as jest.Mock).mockRejectedValue(new Error('Test Error'));
+
+        const response = await request(mockApp).get(testURI);
+
+        expect(response.status).toBe(500);
+        expect(response.text).toBe('Internal Server Error');
+    });
+});

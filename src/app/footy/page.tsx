a9f694a4f23f09726892be42a753645f58288ade
Jon Wilson <github@toastboy.co.uk>
2025-01-18 15:30:42 +0000

    SYS-316 Use Mantine image for crest

--- src/app/footy/page.tsx
+++ src/app/footy/page.tsx
@@ -1,18 +1,17 @@
-import Image from 'next/image';
+import { Box, Image } from "@mantine/core";
 
 type PageProps = object
 
 const Page: React.FC<PageProps> = () => {
     return (
-        <div className="m-auto w-[283px] rounded overflow-hidden shadow-lg" >
+        <Box className="m-auto w-[283px] rounded overflow-hidden shadow-lg" >
             <Image
                 src="/crest.jpg"
                 width={283}
                 height={342}
-                priority={true}
                 alt="Toastboy FC Crest"
             />
-        </div>
+        </Box>
     );
 };
 

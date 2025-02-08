205549a31f6c73ea0410bea57fc47dcdd1ce895e
Jon Wilson <github@toastboy.co.uk>
2025-02-08 17:55:09 +0000

    SYS-399 Mock UserButton in NavbarNested test

--- tests/components/NavBarNested.test.tsx
+++ tests/components/NavBarNested.test.tsx
@@ -2,6 +2,12 @@ import { render } from '@testing-library/react';
 import NavbarNested from 'components/NavBarNested/NavBarNested';
 import { Wrapper } from "./lib/common";
 
+jest.mock('components/UserButton/UserButton', () => {
+    const MockUserButton = () => <div data-testid="mock-user-button" />;
+    MockUserButton.displayName = 'MockUserButton';
+    return MockUserButton;
+});
+
 describe('NavbarNested', () => {
     it('renders navbar header', () => {
         const { container } = render(<Wrapper><NavbarNested /></Wrapper>);

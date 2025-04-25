363a4608fd255bbae719b427b52a6cfb062cf90f
Jon Wilson <github@toastboy.co.uk>
2025-04-25 11:48:51 +0100

    SYS-418 More zod integration

--- prisma/generated/prisma/client/edge.js
+++ prisma/generated/prisma/client/edge.js
@@ -456,7 +456,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/prisma/client/index.js
+++ prisma/generated/prisma/client/index.js
@@ -457,7 +457,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {

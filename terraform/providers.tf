e5b44ec952f62c8036c9e1f2265289e07efebaf8
Jon Wilson <github@toastboy.co.uk>
2025-06-18 07:18:11 +0100

    SYS-429 Update Terraform providers and add to Dependabot updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 

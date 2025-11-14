fd2dc78a589c5bb8e82678ef597f5f00ff3e9272
Jon Wilson <github@toastboy.co.uk>
2025-11-14 06:11:55 +0000

    Formatting

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -6,6 +6,7 @@ import sanitizeHtml from 'sanitize-html';
 // Get sender address and name from environment variables, with sensible defaults
 const MAIL_FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS || 'footy@toastboy.co.uk';
 const MAIL_FROM_NAME = process.env.MAIL_FROM_NAME || 'Toastboy FC Mailer';
+
 /**
  * Sends an email using the appropriate SMTP server configuration based on the environment.
  *
@@ -18,13 +19,13 @@ const MAIL_FROM_NAME = process.env.MAIL_FROM_NAME || 'Toastboy FC Mailer';
  * @throws Will throw an error if the email fails to send.
  */
 export async function sendEmail(to: string, subject: string, html: string) {
-    const transporter = process.env.NODE_ENV === 'production'
-        ? nodemailer.createTransport({
+    const transporter = process.env.NODE_ENV === 'production' ?
+        nodemailer.createTransport({
             host: process.env.SMTP_HOST || 'toastboy-co-uk.mail.protection.outlook.com',
             port: 25,
             secure: false,
-        })
-        : nodemailer.createTransport({
+        }) :
+        nodemailer.createTransport({
             host: 'localhost',
             port: 1025,
             secure: false,

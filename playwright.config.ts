678cc2d3cfa537479f154014afefc27159b40644
Jon Wilson <github@toastboy.co.uk>
2025-11-14 13:07:22 +0000

    SYS-369 Playwright improvements

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run start:ci' : 'npm run dev',
     url: 'http://127.0.0.1:3000',
     // See https://playwright.dev/docs/test-advanced
     reuseExistingServer: !process.env.CI,

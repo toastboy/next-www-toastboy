daa3f96ccba7174cfbcca42c86f5534c7ca6cf52
Jon Wilson <github@toastboy.co.uk>
2025-12-08 10:47:01 +0000

    SYS-302 Rudimentary YearSelector Story

---
+++ src/components/YearSelector/YearSelector.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+import YearSelector from './YearSelector';
+
+const meta = {
+    title: 'Tables/YearSelector',
+    component: YearSelector,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof YearSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        activeYear: 2019,
+        validYears: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
+    },
+};

e37bedddc42c16fdb72167f5ae2138d8a542bf2a
Jon Wilson <github@toastboy.co.uk>
2025-12-08 11:03:37 +0000

    SYS-302 Add Turnout Story

---
+++ src/components/Turnout/Turnout.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+import { defaultTurnoutByYearList } from '@/tests/mocks';
+
+import Turnout from './Turnout';
+
+const meta = {
+    title: 'Tables/Turnout',
+    component: Turnout,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof Turnout>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        turnout: defaultTurnoutByYearList,
+    },
+};

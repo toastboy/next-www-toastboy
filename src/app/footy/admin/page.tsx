cbf28b172af4e28e3a509556880d2c8dd313599f
Jon Wilson <github@toastboy.co.uk>
2026-01-14 07:11:49 +0000

    SYS-480 Some formatting on the admin dashboard

--- src/app/footy/admin/page.tsx
+++ src/app/footy/admin/page.tsx
@@ -1,13 +1,25 @@
+import { Center, Container, Stack, Title } from '@mantine/core';
+
 import { AdminExportAuth } from '@/components/AdminExportAuth/AdminExportAuth';
 import { AdminUpdatePlayerRecords } from '@/components/AdminUpdatePlayerRecords/AdminUpdatePlayerRecords';
 import { MustBeLoggedIn } from '@/components/MustBeLoggedIn/MustBeLoggedIn';
 
 const Page: React.FC = () => {
     return (
-        <MustBeLoggedIn admin={true}>
-            <AdminUpdatePlayerRecords />
-            <AdminExportAuth />
-        </MustBeLoggedIn>
+        <Container size="xs" mt="xl" >
+            <Center>
+                <Title order={2} mb="md" >
+                    Admin Dashboard
+                </Title>
+            </Center>
+
+            <MustBeLoggedIn admin={true}>
+                <Stack mb="lg">
+                    <AdminUpdatePlayerRecords />
+                    <AdminExportAuth />
+                </Stack>
+            </MustBeLoggedIn>
+        </Container >
     );
 };
 

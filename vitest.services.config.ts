807f250683e8dda8d95118f96b0786a04d774541
Jon Wilson <github@toastboy.co.uk>
2026-01-22 12:40:41 +0000

    SYS-488 Move all package.json test scripts over to vitest

--- vitest.services.config.ts
+++ vitest.services.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
         setupFiles: ['vitest.setup.backend.ts'],
         coverage: {
             include: ['src/services/**/*.{ts,tsx}'],
+            exclude: ['**/index.ts'],
         },
     },
 });

e3ddccfdab4aa61cea812ec2265f459f5afae1a2
Jon Wilson <github@toastboy.co.uk>
2026-01-25 09:46:20 +0000

    SYS-488 Formatting of storybook setup and push after commit by default in VS Code

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,35 +1,34 @@
 import "@mantine/core/styles.css";
 
-import React from "react";
+import { MantineProvider } from "@mantine/core";
 import type { Preview } from "@storybook/nextjs-vite";
 import type { Decorator } from "@storybook/react";
-import { MantineProvider } from "@mantine/core";
 
 const mantineDecorator: Decorator = (Story) => (
-  <MantineProvider>
-    <Story />
-  </MantineProvider>
+    <MantineProvider>
+        <Story />
+    </MantineProvider>
 );
 
 const preview: Preview = {
-  decorators: [mantineDecorator],
-  parameters: {
-    nextjs: {
-      appDirectory: true,
+    decorators: [mantineDecorator],
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        a11y: {
+            // 'todo' - show a11y violations in the test UI only
+            // 'error' - fail CI on a11y violations
+            // 'off' - skip a11y checks entirely
+            test: "todo",
+        },
     },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: "todo",
-    },
-  },
 };
 
 export default preview;

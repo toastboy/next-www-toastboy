ad2e24302f51d87326a79502dee49731a1e468c3
Jon Wilson <github@toastboy.co.uk>
2026-03-08 12:53:16 +0000

    SYS-501 Add root page test

--- src/app/page.vitest.spec.ts
+++ src/app/page.vitest.spec.ts
@@ -3,5 +3,12 @@ import { vi } from 'vitest';
 vi.mock('next/navigation');
 
 describe('Root page', () => {
-    it.todo('redirects to /footy');
+    it('redirects to /footy', async () => {
+        const { redirect } = await import('next/navigation');
+        const HomePage = (await import('./page')).default;
+
+        HomePage();
+
+        expect(redirect).toHaveBeenCalledWith('/footy');
+    });
 });

865fc8ce1438e09f1889c54d6db0aa3638c66896
Jon Wilson <github@toastboy.co.uk>
2026-04-06 14:14:52 +0100

    SYS-368 Passing pages tests once more

--- vitest.pages.config.ts
+++ vitest.pages.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vitest/config';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    esbuild: {
+    oxc: {
         jsx: 'automatic',
     },
     resolve: {

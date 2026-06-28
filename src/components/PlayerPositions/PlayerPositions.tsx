dd1d455401f3e28a5cce905e9f1818bdaa53859f
Jon Wilson <github@toastboy.co.uk>
2026-06-28 16:41:07 +0100

    SYS-537 Make PlayerPositions entries into links

--- src/components/PlayerPositions/PlayerPositions.tsx
+++ src/components/PlayerPositions/PlayerPositions.tsx
@@ -1,4 +1,4 @@
-import { Divider, Paper, Table, TableTbody, TableTd, TableTh, TableTr, Title } from '@mantine/core';
+import { Anchor, Divider, Paper, Table, TableTbody, TableTd, TableTh, TableTr, Title } from '@mantine/core';
 import { TableNameSchema } from 'prisma/zod/schemas';
 import type { PlayerRecordType } from 'prisma/zod/schemas/models/PlayerRecord.schema';
 
@@ -25,11 +25,20 @@ export const PlayerPositions = ({ player, year, record }: Props) => {
                         const position = record ?
                             record[rankMap[table][0] as keyof typeof record] ?? null :
                             null;
+                        const href = `/footy/table/${table}?year=${year}`;
 
                         return (
                             <TableTr key={table}>
-                                <TableTh>{table.charAt(0).toUpperCase() + table.slice(1)}</TableTh>
-                                <TableTd w="3rem">{position ?? '-'}</TableTd>
+                                <TableTh>
+                                    <Anchor href={href}>
+                                        {table.charAt(0).toUpperCase() + table.slice(1)}
+                                    </Anchor>
+                                </TableTh>
+                                <TableTd w="3rem">
+                                    {position !== null ?
+                                        <Anchor href={href}>{position}</Anchor> :
+                                        '-'}
+                                </TableTd>
                             </TableTr>
                         );
                     })}
